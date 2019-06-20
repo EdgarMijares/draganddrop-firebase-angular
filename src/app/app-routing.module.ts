1a48b46c7c74d0e7226084fcd596ae8a683e5070
Edgar Mijares <progresssystemsoftware@gmail.com>
2019-06-20 12:16:33 -0500

    Variable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FotosComponent } from './components/fotos/fotos.component';
 import { CargaComponent } from './components/carga/carga.component';
+import { InfoComponent } from './components/info/info.component';
 
 const routes: Routes = [
   { path: 'fotos', component: FotosComponent },
   { path: 'carga', component: CargaComponent },
+  { path: 'info', component: InfoComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'fotos'}
 ];
 
